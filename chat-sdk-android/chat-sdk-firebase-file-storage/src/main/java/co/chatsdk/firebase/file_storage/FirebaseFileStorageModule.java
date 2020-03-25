495074d00f1adbda362164a79add3892a2731dfe
Steffen Klaski <steffen.klaski@t-systems.com>
2020-03-25 15:09:53 +0100

    1

--- chat-sdk-android/chat-sdk-firebase-file-storage/src/main/java/co/chatsdk/firebase/file_storage/FirebaseFileStorageModule.java
+++ chat-sdk-android/chat-sdk-firebase-file-storage/src/main/java/co/chatsdk/firebase/file_storage/FirebaseFileStorageModule.java
@@ -1,6 +1,12 @@
-package co.chatsdk.firebase.file_storage;
+package co.chatsdk.fireebase
 
 import co.chatsdk.core.session.ChatSDK;
+import co.chatsdk.firebase.file_storage.FirebaseUploadHandler;
+
+File Storage(free)
+        Firebase Push Notifications(free)
+        Firebase Social Login(free)
+        Fireabse UI(free)base.file_storage;
 
 /**
  * Created by ben on 9/1/17.
