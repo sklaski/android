7b0acc902cbbc5c53a70ee2f8a225ab2bfe6ca82
steffen.klaski <steffen.klaski@telekom.de>
2020-07-09 14:19:20 +0200

    delete chat-sdk

--- chat-sdk-android_original/chat-sdk-core/src/main/java/co/chatsdk/core/base/AbstractMessageHandler.java
+++
@@ -1,8 +0,0 @@
-package co.chatsdk.core.base;
-
-import co.chatsdk.core.handlers.MessageHandler;
-import co.chatsdk.core.interfaces.MessageDisplayHandler;
-
-public abstract class AbstractMessageHandler implements MessageHandler {
-
-}
